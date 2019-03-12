a66a85a73d27952ae311184ef3cf114f84afb0da
Admin <admin@Admins-iMac-21.local>
2019-03-12 11:37:56 +0700

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Admin on 12/3/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
