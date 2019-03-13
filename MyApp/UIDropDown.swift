c5312084188ba11c61c6af11c6f4d43e1ffdd2ba
Admin <admin@Admins-iMac-21.local>
2019-03-13 15:12:54 +0700

    first commit

---
+++ MyApp/UIDropDown.swift
@@ -0,0 +1,30 @@
+//
+//  UIDropDown.swift
+//  MyApp
+//
+//  Created by Admin on 13/3/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+
+class UIDropDown: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
